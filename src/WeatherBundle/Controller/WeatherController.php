42d6017a36888d249250940794a8cc2319b67d54
Paul Negrerie <paulnegrerie@macbook-pro-de-paul.home>
2019-10-18 20:50:46 +0200

    controller for weather bundle

---
+++ src/WeatherBundle/Controller/WeatherController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\WeatherBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class WeatherController extends AbstractController
+{
+   
+}
